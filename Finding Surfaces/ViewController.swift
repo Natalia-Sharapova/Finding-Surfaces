08903cf0f12c86efe46eca947b211efc6ed664ab
Natalia Sharapova <emuradyants2005@gmail.com>
2021-10-17 19:10:46 +0300

    Initial Commit

---
+++ Finding Surfaces/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  Finding Surfaces
+//
+//  Created by Наталья Шарапова on 16.10.2021.
+//
+
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Set debug options
+        sceneView.debugOptions = [.showFeaturePoints, .showWorldOrigin]
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .horizontal {
+            
+            //set size
+            let extent = planeAnchor.extent
+            let width = CGFloat(extent.x)
+            let height = CGFloat(extent.z)
+            
+            
+            //create geometry
+           let plane = SCNPlane(width: width, height: height)
+            plane.firstMaterial?.diffuse.contents = UIColor.green
+            
+            
+            //create node
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi / 2
+            planeNode.opacity = 0.5
+            
+            //add node to the detected plane
+            node.addChildNode(planeNode)
+        }
+}
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let planeAnchor = anchor as? ARPlaneAnchor,planeAnchor.alignment == .horizontal {
+            guard let planeNode = node.childNodes.first, let plane = planeNode.geometry as? SCNPlane else {
+                return
+            }
+            plane.width = CGFloat(planeAnchor.extent.x)
+            plane.height = CGFloat(planeAnchor.extent.z)
+            
+            planeNode.simdPosition = planeAnchor.center
+            }
+        }
+    }
+
